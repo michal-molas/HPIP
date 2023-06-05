944da65070f73b5b1af1cde1b8924dc8a1ba6a9c
michal-molas <molas.mich@gmail.com>
2023-06-05 10:17:59 +0200

    safe mul

--- Main.hs
+++ Main.hs
@@ -27,7 +27,13 @@ defaultPixelAt default_px img x y =
                     else default_px
 
 safeAdd :: Pixel8 -> Pixel8 -> Pixel8
-safeAdd p1 p2 = if fromIntegral p2 + fromIntegral p1 > 255 then 255 else p1 + p2
+safeAdd p1 p2 = if p1 > 255 - p2 then 255 else p1 + p2
+
+safeSub :: Pixel8 -> Pixel8 -> Pixel8
+safeSub p1 p2 = if p2 > p1 then 0 else p1 - p2
+
+safeMul :: Pixel8 -> Pixel8 -> Pixel8
+safeMul p x = if p > div 255 x then 255 else p * x
 
 castToPixel8 :: Double -> Pixel8
 castToPixel8 value = clamp (round value)
@@ -71,9 +77,6 @@ convolve gauss_size gauss img =
         horizontal = generateImage (convolveHorizontal gauss_size gauss img) width height
             in generateImage (convolveVertical gauss_size gauss horizontal) width height
 
-safeSub :: Pixel8 -> Pixel8 -> Pixel8
-safeSub p1 p2 = if p2 > p1 then 0 else p1 - p2
-
 subtractImages :: Image PixelRGB8 -> Image PixelRGB8 -> Image PixelRGB8
 subtractImages img1 img2 =
     let f = (\x y ->
@@ -90,6 +93,13 @@ addImages img1 img2 =
                 in PixelRGB8 (safeAdd r r') (safeAdd g g') (safeAdd b b'))
     in generateImage f (imageWidth img1) (imageHeight img1)
 
+genAndSave :: Image PixelRGB8 -> String -> IO (Image PixelRGB8)
+genAndSave img name = do
+    savePngImage ("images/" ++ name) (ImageRGB8 img)
+    return img
+
+enhancePixel :: Pixel8 -> PixelRGB8 -> PixelRGB8
+enhancePixel x (PixelRGB8 r g b) = PixelRGB8 (safeMul r x) (safeMul g x) (safeMul b x)
 
 
 -- pomysł: 
@@ -103,8 +113,6 @@ main = do
             [path_, sigma_, factor_] -> return (path_, read sigma_ :: Double, read factor_ :: Pixel8)
             _ -> die "No file given"
 
-    -- let factor = 3
-    -- let sigma = 3.0
     let gauss_size = let x = ceiling (6 * sigma) in if even x then x + 1 else x
     print gauss_size
     let gauss = gaussianKernel sigma gauss_size
@@ -112,23 +120,9 @@ main = do
     case either_img of
         Left s -> die s
         Right dynamic_img -> do
-            let img = convertRGB8 dynamic_img
-            savePngImage "orig.png" (ImageRGB8 img)
-            let blurred = convolve gauss_size gauss img
-            savePngImage "blur.png" (ImageRGB8 blurred)
-            let diff = subtractImages img blurred
-            savePngImage "diff.png" (ImageRGB8 diff)
-            let enhanceddiff = pixelMap (\(PixelRGB8 r g b) -> PixelRGB8 (r * factor) (g * factor) (b * factor)) diff
-            savePngImage "enhanceddiff.png" (ImageRGB8 enhanceddiff)
-            let out = addImages img enhanceddiff
-            savePngImage "out.png" (ImageRGB8 out)
-            -- let grey_img = convertToGS img
-            -- savePngImage "grey.png" (ImageY8 grey_img)
-            -- let conv_img = convolve gauss_size gauss grey_img
-            -- savePngImage "conv.png" (ImageY8 conv_img)
-            -- let diff_img = subtractImagesG grey_img conv_img
-            -- savePngImage "diff.png" (ImageY8 diff_img)
-            -- let stronger_img = diff_img--pixelMap (\x -> if x > 20 then x else 255) diff_img
-            -- savePngImage "adjusted.png" (ImageY8 stronger_img)
-            -- let final_img = subtractImagesRGB img stronger_img
-            -- savePngImage "final.png" (ImageRGB8 final_img)
+            original <- genAndSave (convertRGB8 dynamic_img) "original.png"
+            blurred <- genAndSave (convolve gauss_size gauss original) "blurred.png"
+            diff <- genAndSave (subtractImages original blurred) "diff.png"
+            enhanced_diff <- genAndSave (pixelMap (enhancePixel factor) diff) "enhanced_diff.png"
+            _ <- genAndSave (addImages original enhanced_diff) "final.png"
+            return ()
--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 hpip:
 	mkdir -p build
+	mkdir -p images
 	ghc -package JuicyPixels -Wall --make -outputdir build -o $@ Main.hs
 
 clean:
-	rm -rf build hpip
+	rm -rf build images hpip
 
 .PHONY: clean
