0ba1caeb270e52dcdfd7176c46b1978d29754dde
michal-molas <molas.mich@gmail.com>
2023-06-05 13:08:57 +0200

    added median blur

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ import System.Environment (getArgs)
 import System.Exit (die)
 
 import qualified GaussianBlur as GB
+import qualified MedianBlur as MB
 import Utils
 
 genAndSave :: Image PixelRGB8 -> String -> IO (Image PixelRGB8)
@@ -10,20 +11,51 @@ genAndSave img name = do
     savePngImage ("images/" ++ name) (ImageRGB8 img)
     return img
 
+genAndSave_ :: Image PixelRGB8 -> String -> IO ()
+genAndSave_ img name = savePngImage ("images/" ++ name) (ImageRGB8 img)
+
+sharpenGauss :: DynamicImage -> IO ()
+sharpenGauss dynamic_img = do
+    putStrLn "Input sigma:"
+    sigmastr <- getLine
+    let sigma = read sigmastr :: Double
+    putStrLn "Input enhancement factor:"
+    factorstr <- getLine
+    let factor = read factorstr :: Pixel8
+
+    original <- genAndSave (convertRGB8 dynamic_img) "original.png"
+    blurred <- genAndSave (GB.convolve sigma original) "blurred.png"
+    diff <- genAndSave (subtractImages original blurred) "diff.png"
+    enhanced_diff <- genAndSave (pixelMap (enhancePixel factor) diff) "enhanced_diff.png"
+    genAndSave_ (addImages original enhanced_diff) "final.png"
+
+sharpenMedian :: DynamicImage -> IO ()
+sharpenMedian dynamic_img = do
+    putStrLn "Input radius:"
+    radiusstr <- getLine
+    let radius = read radiusstr :: Int
+    putStrLn "Input enhancement factor:"
+    factorstr <- getLine
+    let factor = read factorstr :: Pixel8
+
+    original <- genAndSave (convertRGB8 dynamic_img) "original.png"
+    blurred <- genAndSave (MB.convolve radius original) "blurred.png"
+    diff <- genAndSave (subtractImages original blurred) "diff.png"
+    enhanced_diff <- genAndSave (pixelMap (enhancePixel factor) diff) "enhanced_diff.png"
+    genAndSave_ (addImages original enhanced_diff) "final.png"
+
 main :: IO ()
 main = do
     args <- System.Environment.getArgs
-    (path, sigma, factor) <- case args of
-            [path_, sigma_, factor_] -> return (path_, read sigma_ :: Double, read factor_ :: Pixel8)
-            _ -> die "No file given"
+    (path, flag) <- case args of
+            [path_, flag_] -> return (path_, flag_)
+            _ -> die "Usage: ./hpip <path> <flag>"
 
     either_img <- readImage path
     case either_img of
         Left s -> die s
         Right dynamic_img -> do
-            original <- genAndSave (convertRGB8 dynamic_img) "original.png"
-            blurred <- genAndSave (GB.convolve sigma original) "blurred.png"
-            diff <- genAndSave (subtractImages original blurred) "diff.png"
-            enhanced_diff <- genAndSave (pixelMap (enhancePixel factor) diff) "enhanced_diff.png"
-            _ <- genAndSave (addImages original enhanced_diff) "final.png"
-            return ()
+            case flag of
+                "G" -> sharpenGauss dynamic_img
+                "M" -> sharpenMedian dynamic_img
+                _ -> die "No such option"
---
+++ src/MedianBlur.hs
@@ -0,0 +1,36 @@
+module MedianBlur where
+
+import Codec.Picture
+import Data.List
+import Utils
+
+getWindowMedian :: Int -> Image PixelRGB8 -> Int -> Int -> PixelRGB8
+getWindowMedian radius img center_x center_y =
+    let start_x = center_x - radius
+        start_y = center_y - radius
+        side = 2 * radius + 1
+    in windowMedian $ go start_x start_y 0 side
+    where
+        go :: Int -> Int -> Int -> Int -> ([Pixel8], [Pixel8], [Pixel8])
+        go start_x start_y idx side
+            | idx == side * side = ([], [], [])
+            | otherwise =
+                let x = start_x + mod idx side
+                    y = start_y + div idx side
+                    PixelRGB8 r g b = defaultPixelAt (PixelRGB8 0 0 0) img x y
+                    (rrest, grest, brest) = go start_x start_y (idx + 1) side
+                in (r:rrest, g:grest, b:brest)
+
+        windowMedian :: ([Pixel8], [Pixel8], [Pixel8]) -> PixelRGB8
+        windowMedian (rs, gs, bs) =
+            let sorted_rs = sort rs
+                sorted_gs = sort gs
+                sorted_bs = sort bs
+                mid = 2 * radius * (radius + 1)
+            in PixelRGB8 (sorted_rs !! mid) (sorted_gs !! mid) (sorted_bs !! mid)
+
+convolve :: Int -> Image PixelRGB8 -> Image PixelRGB8
+convolve radius img =
+    let width = imageWidth img
+        height = imageHeight img
+    in generateImage (getWindowMedian radius img) width height
