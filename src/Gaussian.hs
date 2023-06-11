74e90c0bbc5716527ce10861325b746c5eb3a3d0
michal-molas <molas.mich@gmail.com>
2023-06-11 12:11:47 +0200

    fixed gauss

--- src/Gaussian.hs
+++ src/Gaussian.hs
@@ -21,39 +21,56 @@ gaussianKernel sigma =
 
 convoluteHorizontalHelp :: [Double] -> Image PixelRGB8 -> Int -> Int -> (Double, Double, Double)
 convoluteHorizontalHelp [] _ _ _ = (0.0, 0.0, 0.0)
-convoluteHorizontalHelp (a:gauss) img x y =
+convoluteHorizontalHelp (a:kernel) img x y =
     let width = imageWidth img
         height = imageHeight img
         PixelRGB8 r g b = pixelAt img (adjustIdx width x) (adjustIdx height y)
-        (r', g', b') = convoluteHorizontalHelp gauss img (x + 1) y
+        (r', g', b') = convoluteHorizontalHelp kernel img (x + 1) y
     in (a * fromIntegral r + r', a * fromIntegral g + g', a * fromIntegral b + b')
 
-convoluteHorizontal :: Int -> [Double] -> Image PixelRGB8 -> Int -> Int -> PixelRGB8
-convoluteHorizontal gauss_size gauss img x y =
-    let center = div (gauss_size - 1) 2
-        (r, g, b) = convoluteHorizontalHelp gauss img (x - center) y
+convolutePixelHorizontal :: Int -> [Double] -> Int -> Int -> Image PixelRGB8 -> Int -> Int -> PixelRGB8
+convolutePixelHorizontal kernel_size kernel sub_idx sub_height img x y =
+    let center = div (kernel_size - 1) 2
+        x_ = x - center
+        y_ = sub_idx * sub_height + y
+        (r, g, b) = convoluteHorizontalHelp kernel img x_ y_
     in PixelRGB8 (castToPixel8 r) (castToPixel8 g) (castToPixel8 b)
 
 convoluteVerticalHelp :: [Double] -> Image PixelRGB8 -> Int -> Int -> (Double, Double, Double)
 convoluteVerticalHelp [] _ _ _ = (0.0, 0.0, 0.0)
-convoluteVerticalHelp (a:gauss) img x y =
+convoluteVerticalHelp (a:kernel) img x y =
     let width = imageWidth img
         height = imageHeight img
         PixelRGB8 r g b = pixelAt img (adjustIdx width x) (adjustIdx height y)
-        (r', g', b') = convoluteVerticalHelp gauss img x (y + 1)
+        (r', g', b') = convoluteVerticalHelp kernel img x (y + 1)
     in (a * fromIntegral r + r', a * fromIntegral g + g', a * fromIntegral b + b')
 
-convoluteVertical :: Int -> [Double] -> Image PixelRGB8 -> Int -> Int -> PixelRGB8
-convoluteVertical gauss_size gauss img x y =
-    let center = div (gauss_size - 1) 2
-        (r, g, b) = convoluteVerticalHelp gauss img x (y - center)
+convolutePixelVertical :: Int -> [Double] -> Int -> Int -> Image PixelRGB8 -> Int -> Int -> PixelRGB8
+convolutePixelVertical kernel_size kernel sub_idx sub_height img x y =
+    let center = div (kernel_size - 1) 2
+        x_ = x
+        y_ = sub_idx * sub_height + y - center
+        (r, g, b) = convoluteVerticalHelp kernel img x_ y_
     in PixelRGB8 (castToPixel8 r) (castToPixel8 g) (castToPixel8 b)
 
-convolute :: Double -> Image PixelRGB8 -> Image PixelRGB8
-convolute sigma img =
+convoluteHorizontal :: Double -> Int -> Image PixelRGB8 -> Int -> Image PixelRGB8
+convoluteHorizontal sigma num_sub img sub_idx =
     let width = imageWidth img
         height = imageHeight img
-        gauss = gaussianKernel sigma
-        gauss_size = length gauss
-        horizontal = generateImage (convoluteHorizontal gauss_size gauss img) width height
-            in generateImage (convoluteVertical gauss_size gauss horizontal) width height
+        extra_pixels = mod height num_sub
+        sub_height = div height num_sub + if sub_idx == num_sub - 1 then extra_pixels else 0
+        
+        kernel = gaussianKernel sigma
+        kernel_size = length kernel
+    in generateImage (convolutePixelHorizontal kernel_size kernel sub_idx sub_height img) width sub_height
+
+convoluteVertical :: Double -> Int -> Image PixelRGB8 -> Int -> Image PixelRGB8
+convoluteVertical sigma num_sub img sub_idx =
+    let width = imageWidth img
+        height = imageHeight img
+        extra_pixels = mod height num_sub
+        sub_height = div height num_sub + if sub_idx == num_sub - 1 then extra_pixels else 0
+        
+        kernel = gaussianKernel sigma
+        kernel_size = length kernel
+    in generateImage (convolutePixelVertical kernel_size kernel sub_idx sub_height img) width sub_height
